cd9d4170b59bac49ffc91f39dbf5802f466b2f43
Camille <contact@camillebourgois.com>
2020-04-14 15:54:42 +0200

    Ajout de cookieService : interactions avec la db

--- src/app/services/cookies.service.ts
+++ src/app/services/cookies.service.ts
@@ -1,20 +1,23 @@
+import { Cookie } from './../models/cookie.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Subject } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CookiesService {
 
-  cookiesSubject = new Subject<any[]>();
-  private cookies = [];
+  cookiesSubject = new Subject<Cookie[]>();
+  private cookies: Cookie[] = [];
+
+  dbUrl: string = 'https://cookiestock-7d25f.firebaseio.com/cookies.json'; 
 
   constructor(private httpClient: HttpClient) { }
 
   saveCookies() {
     this.httpClient
-      .post('https://cookiestock-7d25f.firebaseio.com/cookies.json', this.cookies)
+      .post(this.dbUrl, this.cookies)
       .subscribe(
         () => {
           console.log('Enregistrement terminé !');
@@ -24,4 +27,32 @@ export class CookiesService {
         }
       );
   }
+
+  emitCookies() {
+    this.cookiesSubject.next(this.cookies);
+  }
+
+  getCookies(): Observable<Cookie[]> {
+    return this.httpClient.get<Cookie[]>(this.dbUrl);
+  }
+
+  createNewCookie(newCookie: Cookie) {
+    this.cookies.push(newCookie);
+    this.saveCookies;
+    this.emitCookies;
+  }
+
+  removeCookie(cookie: Cookie) {
+    const cookieIndexToRemove = this.cookies.findIndex(
+      (cookieEl) => {
+        if(cookieEl === cookie) {
+          return true;
+        }
+      }
+    );
+    this.cookies.splice(cookieIndexToRemove, 1);
+    this.saveCookies;
+    this.emitCookies;
+  }
+
 }
